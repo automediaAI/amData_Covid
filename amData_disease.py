b23d758bd133eb005b29ab8716248c72e97f5435
Nipun Khanna <2Nipun@gmail.com>
2021-03-28 17:15:05 -0700

    wip - pull data per airtable like news
    wip push - not working but code all done

--- amData_disease.py
+++ amData_disease.py
@@ -18,13 +18,16 @@ client = diseaseapi.Client().covid19
 ### DATA PULL FUNCTIONS
 
 # Function to get data of all countries in a single call 
-async def get_allCountries():
+# async def get_allCountries(queryName):
+def get_allCountries(queryName):
 	dataFormatted = []
-	dataList = await client.all_countries() #get for a country
+	# dataList = await client.all_countries() #get for a country
+	dataList = client.all_countries() #get for a country
 	for x in range(len(dataList)):
 		data = dataList[x]
 		dataFormatted.append({
 			"areaTable":"CountryData",
+			"query_name": queryName,   #Name of record in amPayload table
 			"region": data.name,
 			"Cases":int(data.cases),
 			"Deaths":int(data.deaths),
@@ -39,16 +42,20 @@ async def get_allCountries():
 			"sourceName": "Source: Worldometer / disease.sh", 
 		})
 	# await client.request_client.close() #close the ClientSession
+	client.request_client.close() #close the ClientSession
 	return dataFormatted #List goes out
 
 # Function to get data of all states of USA 
-async def get_allStates():
+# async def get_allUSStates(queryName):
+def get_allUSStates(queryName):
 	dataFormatted = []
-	dataList = await client.all_states() #get for a country
+	# dataList = await client.all_states() #get for a country
+	dataList = client.all_states() #get for a country
 	for x in range(len(dataList)):
 		data = dataList[x]
 		dataFormatted.append({
 			"areaTable":"StateData_US",
+			"query_name": queryName,   #Name of record in amPayload table
 			"region": data.name,
 			"Cases":int(data.cases),
 			"Deaths":int(data.deaths),
@@ -63,14 +70,20 @@ async def get_allStates():
 			"sourceName": "Source: Worldometer / disease.sh", 
 		})
 	# await client.request_client.close() #close the ClientSession
+	client.request_client.close() #close the ClientSession
 	return dataFormatted #List goes out
 
 #Function for pulling data for world, for all else use other of all data
-async def get_WorldData():
+# async def get_WorldData(queryName):
+def get_WorldData(queryName):
 	dataFormatted = {}
-	data = await client.all() #get global data
+	print ("Client", client)
+	# data = await client.all() #get global data
+	data = client.all() #get global data
+	print ("data: ", data)
 	dataFormatted.update ({
 			"areaTable":"CountryData",
+			"query_name": queryName,   #Name of record in amPayload table
 			"region": "World",
 			"Cases":int(data.cases),
 			"Deaths":int(data.deaths),
@@ -85,19 +98,30 @@ async def get_WorldData():
 			"sourceName": "Source: Worldometer / disease.sh", 
 	})
 	
-	await client.request_client.close() #close the ClientSession
-	return dataFormatted #Single JSON goes out
+	# await client.request_client.close() #close the ClientSession
+	client.request_client.close() #close the ClientSession
+	dataFormatted_list = [dataFormatted] #JSON to list 
+	return dataFormatted_list #Single List goes out
 
 
-def get_DiseaseData():
-	dataAll = asyncio.get_event_loop().run_until_complete(get_allCountries()) #All countries
-	print ("Data Gathered for: All Countries ..")
-	dataAll.extend(asyncio.get_event_loop().run_until_complete(get_allStates())) #All US States
-	print ("Data Gathered for: All US States ..")
-	dataAll.append(asyncio.get_event_loop().run_until_complete(get_WorldData())) #Only World Data
-	print ("Data Gathered for: World ..")
-	print ("Data Gathered for all.")
-	#Client connection closing is done only on last API call within the functions
+def get_DiseaseData(payload_json, query_name):
+	endpoint_name = payload_json['endpoint']
+	if endpoint_name == "WorldData":
+		data_pulled = get_WorldData(query_name)
+		print ("World data", data_pulled)
+	elif endpoint_name == "allCountries":
+		data_pulled = get_allCountries(query_name)
+	elif endpoint_name == "allUSStates":
+		data_pulled = get_allUSStates(query_name)
+	else:
+		data_pulled = []
+	return data_pulled
 
-	return dataAll
+	# dataAll = asyncio.get_event_loop().run_until_complete(get_allCountries()) #All countries
+	# dataAll.extend(asyncio.get_event_loop().run_until_complete(get_allUSStates())) #All US States
+	# dataAll.append(asyncio.get_event_loop().run_until_complete(get_WorldData())) #Only World Data
+	# print ("Data Gathered for: World ..")
+	# print ("Data Gathered for all.")
+	# #Client connection closing is done only on last API call within the functions
+	# return dataAll
 
--- task.py
+++ task.py
@@ -7,35 +7,111 @@
 #############
 
 import os
-from amData_disease import get_DiseaseData
-from amAirtable_disease import updateLoop
-from airtable import Airtable
+import json
 import uuid
-from datetime import datetime
+import boto3 #to upload larger files to S3
+from botocore.exceptions import ClientError
+from airtable import Airtable
+from datetime import date, datetime, timedelta
+from amData_disease import get_DiseaseData
 
 # Airtable settings 
 base_key = os.environ.get("PRIVATE_BASE_KEY")
-table_name_dump = os.environ.get("PRIVATE_TABLE_NAME_DUMP")
-api_key = os.environ.get("PRIVATE_API_KEY")
-airtable_2 = Airtable(base_key, table_name_dump, api_key)
+# table_name_data = os.environ.get("PRIVATE_TABLE_NAME_DATAPAYLOAD") #What to pull
+table_name_data = "amPayload_Data" #What to pull
+table_name_dump = os.environ.get("PRIVATE_TABLE_NAME_SERVICEDUMP") #Output dump
+api_key_airtable = os.environ.get("PRIVATE_API_KEY")
+airtable_data = Airtable(base_key, table_name_data, api_key_airtable)
+airtable_dump = Airtable(base_key, table_name_dump, api_key_airtable)
 
-## Making API call to get data 
-dataPulled = get_DiseaseData()
+## Amazon S3 settings 
+# AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+# AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_ACCESS_KEY_ID = 'AKIAJG2RATLX4UP4SG5A'
+AWS_SECRET_ACCESS_KEY = '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' 
+aws_region='us-west-1' #Manual while creating the bucket 
 
-## Payload for uploading dump to airtable
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+)
+
 UUID = 'CovidData-'+str(uuid.uuid1())
-time_pulled = str(datetime.now())
-input_payload = "All Countries, All States of USA"
-amService = 'amData_Covid'
-fields = {'UUID':UUID, 'time_pulled':time_pulled, 'data_output': str(dataPulled), 'input_payload':input_payload, 'amService':amService }
-# Upload
-airtable_2.insert(fields)
-print("Data uploaded to dump table.")
 
-## Uploading real data to CMS
-updateLoop(dataPulled)
-print("Data updated in CMS per query.")
+#Uploads single json, or list to data_output of record ID as given
+def uploadData(inputDictList, recToUpdate):
+	recID = recToUpdate
+	if isinstance(inputDictList, dict):
+		fields = {'output': json.dumps(inputDictList)}
+		# fields = {'data_output': str(inputDictList)} #Seems if I do str thats same too
+	else:
+		fields = {'output': str(inputDictList)}
+	airtable_data.update(recID, fields)
 
-## Testing
-# print ('Data Pulled: ', dataPulled)
-# print ('Data Pulled Type: ', type(dataPulled))
+# Dumping to service Dump after all is run
+def dumpData(inputURL):
+	time_pulled = str(datetime.now())
+	amService = 'amData_CovidData'
+	data_output = str(inputURL)
+	fields = {'UUID':UUID, 'time_pulled':time_pulled, 'data_output': data_output, 'amService':amService }
+	airtable_dump.insert(fields)
+
+# # Dumping to service Dump after all is run
+def dumpToS3(file_name, bucket='amnewsbucket', object_name=None):
+    # If S3 object_name was not specified, use file_name
+    url_s3 = f"https://{bucket}.s3-us-west-2.amazonaws.com/{file_name}" #Manually creating structure
+    object_name = file_name
+    try:
+        response = s3.upload_file(file_name, bucket, object_name, ExtraArgs={'ACL':'public-read'})
+        return url_s3
+    except ClientError as e:
+        return ('🚫Error uploading to S3: '+str(e))
+ 
+
+# Running through rows of news, calling newsAPI, uploading data back
+def updateDataLoop():
+	print ('Started loop..') #Extra to keep app going 
+	table_output = [] #Final data of entire pull
+	allRecords = airtable_data.get_all() #Get all records 
+	print ('All records recieved..') #Extra to keep app going 
+	for i in allRecords:
+		if "Prod_Ready" in i["fields"]: #Only working on prod ready ie checkboxed
+			print ('Started row..') #Extra to keep app going 
+			if "Service" in i["fields"]:
+				# Basic payload, common to all
+				payload_native = i["fields"]["payload"]
+				payload_json = json.loads(payload_native)
+				rec_ofAsked = i["id"]
+				query_name = i["fields"]["Name"] #Just to differentiate what is being called
+				endpoint_name = i["fields"]["Service"].lower()
+				# Calling News service per ask
+				if endpoint_name == 'diseasesh': #Only pulling if NewsAPI 	
+					print ("Entered loop")
+					row_output_unclean = get_DiseaseData(payload_json, query_name) #NewsAPI output for this call
+					row_output = row_output_unclean
+					# row_output = newsClean(row_output_unclean)
+				else:
+					row_output = "🚫Query requested is invalid"
+				# Appending rest
+				print ('Row data done..') #Extra to keep app going 
+				table_output.append(row_output) #Adding to all data
+				## Running Text Cleaning and Image Cleaning functions 
+				data_toUpload = row_output #Uploading clean data
+				# data_toUpload = row_output #Uploading clean data
+				uploadData(data_toUpload, rec_ofAsked) #Upload back to Airtable 
+				print('Row complete..')
+	
+	filename = (UUID+'.txt')
+	#Creating a local text file 
+	f = open(filename,"w")
+	f.write( str(table_output) )
+	f.close()
+	url_s3_file = dumpToS3(filename) #uploading to S3 and getting file back
+	dumpData(url_s3_file) #Adding final output to service dump
+	os.remove(filename) #deleting file after upload
+	print('Table complete.')
+
+print ('Entering loop..')
+
+updateDataLoop()
